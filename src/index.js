0f2e0e6015ff3fd243e0b46b65569ab0f8f0f82d
Elena Cristescu <elenacristescu11@gmail.com>
2024-02-15 15:58:24 +0100

    render a list for the pizza components using the map method

--- src/index.js
+++ src/index.js
@@ -64,7 +64,7 @@ function Header() {
   return (
     <div>
       <header className="header">
-        <h1 style={style}>Fast React Pizza Co.</h1>
+        <h1 style={style}> Fast React Pizza Co. </h1>
       </header>
     </div>
   );
@@ -74,7 +74,12 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <Pizza
+      <ul className="pizzas">
+        {pizzaData.map((pizza) => (
+          <Pizza pizzaObj={pizza} key={pizza.name} />
+        ))}
+      </ul>
+      {/* <Pizza
         name="Pizza Spinaci"
         ingredients="Tomato, mozarella, spinach, and ricotta cheese"
         photoName="pizzas/spinaci.jpg"
@@ -85,20 +90,20 @@ function Menu() {
         ingredients="Tomato, mozarella, mushrooms, and onion"
         photoName="pizzas/funghi.jpg"
         price={12}
-      />
+      /> */}
     </main>
   );
 }
 function Pizza(props) {
   return (
-    <div className="pizza">
-      <img src={props.photoName} alt={props.name} />
+    <li className="pizza">
+      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
       <div>
-        <h3>{props.name}</h3>
-        <p>{props.ingredients}</p>
-        <span>{props.price}</span>
+        <h3>{props.pizzaObj.name}</h3>
+        <p>{props.pizzaObj.ingredients}</p>
+        <span>{props.pizzaObj.price}</span>
       </div>
-    </div>
+    </li>
   );
 }
 
